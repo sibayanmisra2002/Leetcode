1615d0f1e70a5353dc1fce8142acbf26747fdb21
Sibayan Misra <sibayanmisra2002@gmail.com>
2024-09-09 20:16:13 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (21.13%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -2,12 +2,21 @@ class Solution {
     public String toLowerCase(String s) {
         // return s.toLowerCase(); // method 1
         //method 2
-        char[] c = s.toCharArray();        
-        for(int i=0; i<c.length; i++) {
-            if(c[i] >= 65 && c[i] <= 90) {
-                c[i] = (char) (c[i] + ('a' - 'A'));
+        // char[] c = s.toCharArray();        
+        // for(int i=0; i<c.length; i++) {
+        //     if(c[i] >= 65 && c[i] <= 90) {
+        //         c[i] = (char) (c[i] + ('a' - 'A'));
+        //     }
+        // }
+        // return String.valueOf(c);
+        
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()) {
+            if(c >= 65 && c <= 90) {
+                c = (char) (c - 'A' + 'a');
             }
+            sb.append(c);
         }
-        return String.valueOf(c);
+        return sb.toString();
     }
 }
